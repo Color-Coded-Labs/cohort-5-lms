114c1ed32b70b18302e039ff41995dc046e0eac0
muayad mustafa <muayad.mustafa82@gmail.com>
2023-09-05 21:06:23 -0400

    added new user, password user match, and delete user function

--- server/index.js
+++ server/index.js
@@ -31,10 +31,60 @@ const User = require("./models/User");
 // ===================== User Routes =====================
 
 app.post("", async (req, res) => {
+  app.post('/register', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+  
+      // Check if username already exists
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        return res.status(400).json({ message: "Username already exists" });
+      }
+  
+      // Hash the password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+  
+      // Create new user
+      const newUser = new User({
+        username,
+        password: hashedPassword
+      });
+  
+      await newUser.save();
+  
+      res.status(200).json({ message: "User registered successfully" });
+  
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
   // TODO: Implement user signup logic
 });
 
 app.post("", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    // Compare password
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    res.status(200).json({ message: "Logged in successfully" });
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
   // TODO: Implement user login logic
 });
 
@@ -42,9 +92,27 @@ app.put("", async (req, res) => {
   // TODO: Implement user update logic
 });
 
-app.delete("", async (req, res) => {
+app.delete("/user/:userId", async (req, res) => {
   // TODO: Implement user deletion logic
+  try {
+    const { userId } = req.params.userId;
+    const result = await User.findByIdAndDelete(userId);
+  
+  res.status(200).json({ message: "User successfully deleted" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
 });
+
+
+    
+
+    //delete user
+    
+
+  // TODO: Implement user deletion logic
+
 
 // ===================== Module Routes =====================
 
