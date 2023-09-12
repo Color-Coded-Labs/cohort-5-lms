1113de726a9abbdc4ef7f529bd3416ede52ce1cc
cleven brown <hinokamikagustuchi@gmail.com>
2023-09-12 19:48:02 -0400

    implimented login screen use state

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
-  return <h1>Existing Users Login</h1>;
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  function handleUsername(e) {
+    setUsername(e.target.value);
+  }
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+
+  async function handleSubmit() {
+    console.log("send username and password to API");
+    console.log("Username:", username);
+    console.log("password:", password);
+    let response = await axios.post(
+      "http://localhost:8080/api/users/login",
+
+      {
+        username,
+        password,
+      }
+    );
+    console.log(response.data)
+  }
+
+  return (
+    <div>
+      <h1>Login!</h1>
+      <form>
+        <label>Username</label>
+        <input type="text" onChange={handleUsername}>Username</input>
+        <label>Password</label>
+        <input type="text" onChange={handlePassword}>Password</input>
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+    </div>
+  )
+} gg
 
 export default Login;
