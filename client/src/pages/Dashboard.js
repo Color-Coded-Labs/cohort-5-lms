3f0682199e7245ed6ab827b4cf5e134568f7341a
Chris Barrett <chrisbarrett312@gmail.com>
2023-09-12 20:42:08 -0400

    dashboard displays array content

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
-  return <h1>View after login -- See modules + topics</h1>;
+  const [modules, setModules] = useState([
+    {
+      _id: "64ed16233b4b18cd5289ea5b",
+      name: "JavaScript Basics",
+      description: "Foundational concepts of JavaScript.",
+      topics: [
+        {
+          title: "Variables and Data Types",
+          content: "Understanding variables and data types in JS.",
+          links: [
+            "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types",
+          ],
+          videos: ["https://www.youtube.com/watch?v=c-xiaCg6XeA"],
+          _id: "64ed16233b4b18cd5289ea5c",
+        },
+        {
+          title: "Functions",
+          content: "How to create and utilize functions.",
+          links: [
+            "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions",
+          ],
+          videos: ["https://www.youtube.com/watch?v=oB5rH_9bqAI"],
+          _id: "64ed16233b4b18cd5289ea5d",
+        },
+      ],
+      __v: 0,
+    },
+    {
+      _id: "64ed16233b4b18cd5289ea58",
+      name: "Web Basics",
+      description: "Foundational web development concepts.",
+      topics: [
+        {
+          title: "HTML",
+          content: "Introduction and basics of HTML.",
+          links: ["https://developer.mozilla.org/en-US/docs/Web/HTML"],
+          videos: ["https://www.youtube.com/watch?v=UB1O30fR-EE"],
+          _id: "64ed16233b4b18cd5289ea59",
+        },
+        {
+          title: "CSS",
+          content: "Styling web pages using CSS.",
+          links: ["https://developer.mozilla.org/en-US/docs/Web/CSS"],
+          videos: ["https://www.youtube.com/watch?v=yfoY53QXEnI"],
+          _id: "64ed16233b4b18cd5289ea5a",
+        },
+      ],
+      __v: 0,
+    },
+  ]);
+
+  return (
+    <div className="dashboard">
+      {modules.map((module) => (
+        <div className="module" key={module._id}>
+          <h2>{module.name}</h2>
+          <p>{module.description}</p>
+          <ul>
+            {module.topics.map((topic) => (
+              <li key={topic._id}>
+                <Link to={`/dashboard/${topic._id}`}>{topic.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Dashboard;
