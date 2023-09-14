3e64e8f930f4c15a01045c20bf64b73f224e46b6
bilaal <bilaal02ismail@gmail.com>
2023-09-14 19:54:31 -0400

    update

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -10,7 +10,7 @@ const Signup = () => {
   const handleSubmit = async () => {
     try {
       const response = await axios.post(
-        "http://localhost:3000/signup",
+        "http://localhost:3000/api/users/signup",
         {
           username,
           password,
@@ -21,39 +21,37 @@ const Signup = () => {
         navigate.push("/login");
       }
     } catch (error) {
-      setErrorMessage("Signup")
       console.log("Signup error", error);
     }
   };
 
   return (
+
     <div>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
         <div className="bg-white p-3 rounded" style={{ width: '40%' }}>
           <h2 className='mb-3 text-primary'>Sign Up</h2>
           <form onSubmit={handleSubmit}>
-            <div className="mb-3 text-start">
-              <label htmlFor="exampleInputEmail1" className="form-label">
+            <div >
+              <label className="form-label">
                 <strong>Username</strong>
               </label>
               <input
                 type="username"
                 placeholder="Enter Username"
                 className="form-control"
-                id="exampleInputusername1"
                 onChange={(event) => setUsername(event.target.value)}
                 required
               />
             </div>
-            <div className="mb-3 text-start">
-              <label htmlFor="exampleInputPassword1" className="form-label">
+            <div>
+              <label className="form-label">
                 <strong>Password</strong>
               </label>
               <input
                 type="password"
                 placeholder="Enter Password"
                 className="form-control"
-                id="exampleInputPassword1"
                 onChange={(event) => setPassword(event.target.value)}
                 required
               />
@@ -63,6 +61,7 @@ const Signup = () => {
         </div>
       </div>
     </div>
+
   )
 }
 
